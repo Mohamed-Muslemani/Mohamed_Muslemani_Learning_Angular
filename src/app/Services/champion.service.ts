a63cd240a5699684274165b69bd1f0d6dc60bf62
Mohamed Muslemani <w0865765@myscc.ca>
2024-10-02 11:03:32 -0400

    Get mock data to service Dependency injection of service to championList component

--- src/app/Services/champion.service.ts
+++ src/app/Services/champion.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {Champions} from "../Shared/Modules/champions";
+import {championList} from "../Shared/mockChampion.data";
+import {Observable, of} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChampionService {
+  private champions: Champions[] = championList
 
   constructor() { }
+
+  getChampions(): Observable<Champions[]>{
+    return of(championList);
+  }
 }
--- src/app/champion-list/champion-list.component.ts
+++ src/app/champion-list/champion-list.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Champions } from '../Shared/Modules/champions';
 import {NgForOf} from "@angular/common";
 import { ChampionDetailsComponent } from "../champion-details/champion-details.component";
+import {ChampionService} from "../Services/champion.service";
 
 @Component({
   selector: 'app-champion-list',
@@ -11,6 +12,8 @@ import { ChampionDetailsComponent } from "../champion-details/champion-details.c
   styleUrl: './champion-list.component.css'
 })
 export class ChampionListComponent {
+  constructor(private championService: ChampionService) {
+  }
   toggleOPStatus(champion: Champions): void {
     champion.isOP = !champion.isOP;
   }
