5ff5ba72beb42d774886756efcf6c233bfbc2ec8
Mohamed Muslemani <w0865765@myscc.ca>
2024-10-08 09:50:42 -0400

    Set up router in main.ts Define a list of routes Add a route to champion-list component

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {provideRouter, Routes } from "@angular/router";
+import {ChampionListComponent} from "./app/champion-list/champion-list.component";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const routes: Routes = [
+  {path:'', redirectTo: '/champions', pathMatch: 'full'},
+  {path: 'champions', component: ChampionListComponent}
+];
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)]
+}).then(r => console.log('Bootstrap successful'));
