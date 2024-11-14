368f3b0d4beac755bf341f4b9f0c4a94bc608a2c
Mohamed Muslemani <w0865765@myscc.ca>
2024-11-14 10:54:38 -0500

    Add lazy loading

--- src/main.ts
+++ src/main.ts
@@ -11,12 +11,23 @@ import {HttpClientInMemoryWebApiModule} from "angular-in-memory-web-api";
 import {InMemoryDataService} from "./app/Services/in-memory-data.service";
 
 const routes: Routes = [
-  {path:'', redirectTo: '/champion', pathMatch: 'full'},
+  {path:'', redirectTo: '/champions', pathMatch: 'full'},
   {path: 'champions', component: ChampionListComponent},
-  {path: 'champions/:id', component: ChampionDetailsComponent},
-  {path: 'modify-champion', component: ModifyListItemComponent},
-  {path: 'modify-champion/:id', component: ModifyListItemComponent},
-  {path: '**', component: PageNotFoundComponent}
+  {path: 'champions/:id', loadComponent: () =>
+      import('./app/champion-details/champion-details.component').then(
+        m=>m.ChampionDetailsComponent
+      )},
+  {path: 'modify-champion', loadComponent: () =>
+      import('./app/modify-list-item/modify-list-item.component').then(
+        m=>m.ModifyListItemComponent
+      )},
+  {path: 'modify-champion/:id', loadComponent: () =>
+      import('./app/modify-list-item/modify-list-item.component').then(
+        m=>m.ModifyListItemComponent
+      )},
+  {path: '**', loadComponent: () =>
+      import('./app/page-not-found/page-not-found.component').then(
+        m => m.PageNotFoundComponent) }
 ];
 
 bootstrapApplication(AppComponent, {
